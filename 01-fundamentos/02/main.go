f869622cda155c8bb68a46b7f7ba93b11f458e24
Guilherme Palma <guilherme.palma.ext@dasa.com.br>
2024-01-16 14:49:43 -0300

    Declaração e atribuição

---
+++ 01-fundamentos/02/main.go
@@ -0,0 +1,23 @@
+package main
+
+// Declaração GLOBAL
+var (
+	b bool
+	c int
+	d string
+	e float32
+)
+
+func main() {
+
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println("Atribuição...")
+	b = true
+	println(b)
+	a:="Declaração shorthand"
+	println(a)
+
+}
